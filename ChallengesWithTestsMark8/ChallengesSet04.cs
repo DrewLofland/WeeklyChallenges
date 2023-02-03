a24441871b061240f2442cd6012119073b48b4ef
Drew Lofland <alofland25@gmail.com>
2023-02-03 10:59:27 -0500

    finished challenge set 4

--- ChallengesWithTestsMark8/ChallengesSet04.cs
+++ ChallengesWithTestsMark8/ChallengesSet04.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace ChallengesWithTestsMark8
 {
@@ -6,47 +8,110 @@ namespace ChallengesWithTestsMark8
     {
         public int AddEvenSubtractOdd(int[] numbers)
         {
-            throw new NotImplementedException();
+            int answer = 0;
+            foreach(int number in numbers)
+            {
+                if(number % 2 == 0) 
+                {
+                    answer += number;
+                }
+                else
+                {
+                    answer -= number;
+                }
+            }
+            return answer;
+            //throw new NotImplementedException();
         }
 
         public int GetLengthOfShortestString(string str1, string str2, string str3, string str4)
         {
-            throw new NotImplementedException();
+            var lengths = new int[] { str1.Length, str2.Length, str3.Length, str4.Length };
+            return lengths.Min();
+            
+            //throw new NotImplementedException();
         }
 
         public int GetSmallestNumber(int number1, int number2, int number3, int number4)
         {
-            throw new NotImplementedException();
+            var nums = new int[] { number1, number2, number3, number4, };
+            return nums.Min();
+            //throw new NotImplementedException();
         }
 
         public void ChangeBusinessNameTo_TrueCoders(Business biz)
         {
-            throw new NotImplementedException();
+            biz.Name = "TrueCoders";
+            //throw new NotImplementedException();
         }
 
         public bool CouldFormTriangle(int sideLength1, int sideLength2, int sideLength3)
         {
-            throw new NotImplementedException();
+            if (sideLength1 <= 0 || sideLength2 <= 0 || sideLength3 <= 0)
+            {
+                return false;
+            }
+            return sideLength1 + sideLength2 > sideLength3 && sideLength1 + sideLength3 > sideLength2 && sideLength2 + sideLength3 > sideLength1;
+
+            //throw new NotImplementedException();
         }
 
         public bool IsStringANumber(string input)
         {
-            throw new NotImplementedException();
+            double answer;
+            return double.TryParse(input, out answer);
+            //throw new NotImplementedException();
         }
 
         public bool MajorityOfElementsInArrayAreNull(object[] objs)
         {
-            throw new NotImplementedException();
+            var nulls = 0;
+            foreach(var item in objs)
+            {
+                if(item == null)
+                {
+                    nulls++;
+                }
+            }
+            return nulls > objs.Length / 2;
+            //throw new NotImplementedException();
         }
 
         public double AverageEvens(int[] numbers)
         {
-            throw new NotImplementedException();
+            double evens = 0;
+            double sum = 0;
+            if(numbers == null)
+            {
+                return 0;
+            }
+            foreach(var item in numbers)
+            {
+                if(item % 2 == 0)
+                {
+                    evens++;
+                    sum += item;
+                }
+                
+            }
+            return evens == 0 ? 0 : sum / evens;
+            //throw new NotImplementedException();
         }
 
         public int Factorial(int number)
         {
-            throw new NotImplementedException();
+
+            var factorial = 1;
+            if(number < 0)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            for(int i = number; i > 0; i--)
+            {
+                factorial *= i;
+            }
+            return factorial;
+            //throw new NotImplementedException();
         }
     }
 }
